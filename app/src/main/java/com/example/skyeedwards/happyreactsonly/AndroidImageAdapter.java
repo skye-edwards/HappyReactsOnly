4ab6af0497274b6f82fa7ed4cd7489e6d33721ab
sky-wardbound <s5edward@ucsd.edu>
2017-04-09 11:17:23 -0700

    Initial commit

---
+++ app/src/main/java/com/example/skyeedwards/happyreactsonly/AndroidImageAdapter.java
@@ -0,0 +1,45 @@
+package com.example.skyeedwards.happyreactsonly;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class AndroidImageAdapter extends PagerAdapter {
+    Context mContext;
+
+    AndroidImageAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return sliderImagesId.length;
+    }
+
+    private int[] sliderImagesId = new int[]{
+            R.drawable.kinetic, R.drawable.pig1, R.drawable.pig3,
+            R.drawable.images, R.drawable.g1380729488540213602, R.drawable.pig2, R.drawable.kinetic1,
+    };
+
+    @Override
+    public boolean isViewFromObject(View v, Object obj) {
+        return v == ((ImageView) obj);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int i) {
+        ImageView mImageView = new ImageView(mContext);
+        mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        mImageView.setImageResource(sliderImagesId[i]);
+        ((ViewPager) container).addView(mImageView, 0);
+        return mImageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int i, Object obj) {
+        ((ViewPager) container).removeView((ImageView) obj);
+    }
+}
